d6c1d9e295e27c7633690305c3a0253926b66451
dozzarn <dogu.1769@gmail.com>
2018-12-19 00:18:37 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.IO;
+using Xabe.FFmpeg;
+using System.Diagnostics;
+namespace ConsoleApp1
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            void exec(string comm)
+            {
+                Process Process = new Process();
+                ProcessStartInfo ProcessInfo;
+                ProcessInfo = new ProcessStartInfo("cmd.exe", "/C " + comm);
+                ProcessInfo.CreateNoWindow = true;
+                ProcessInfo.UseShellExecute = false;
+                Process = Process.Start(ProcessInfo);
+                Process.WaitForExit();
+                Process.Close();
+            }
+
+            int[] start = new int[] { 5, 10, 15 };
+            int time = 5;
+            for (int i = 0; i < start.Length; i++)
+            {
+                string arguments = $"ffmpeg -ss {start[i]} -i C:\\Users\\Dogukan\\Desktop\\a.mp4 -t {time} -c copy output\\{i}.mp4";
+                exec(arguments);
+            }
+        }
+
+  
+
+    }
+}
